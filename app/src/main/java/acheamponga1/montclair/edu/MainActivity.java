63f12127a455910fe75c0fa362bfb292b04194a8
yungcarter30 <acheamponga1@montclair.edu>
2020-01-31 18:51:04 -0500

    Initial commit

---
+++ app/src/main/java/acheamponga1/montclair/edu/MainActivity.java
@@ -0,0 +1,14 @@
+package acheamponga1.montclair.edu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
